c8f18fea7c92ffe08c7470a65d387ba6e0a36427
CarlosAlvaZ <carlos.alvarenga017@gmail.com>
2023-11-10 16:36:25 -0600

    added types

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 8080 || process.env.PORT
 
 const wss = new WebSocketServer({ port: port });
 
+console.log("Working!")
+
 wss.on('connection', function connection(ws) {
   ws.on('message', function message(data) {
     console.log('received: %s', data);
